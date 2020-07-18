13aa37c40ac71743c403bdc300909acac11c28ba
fahdikrie <fahdiaf@gmail.com>
2020-07-19 06:46:50 +0700

    checkpoint sblm migrating ke redux

---
+++ src/containers/PokeFeed/reducer.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-case-declarations */
+/* eslint-disable array-callback-return */
+import { fromJS } from "immutable";
+import {
+  FETCH_POKEMON_LIST,
+  FETCH_POKEMON_LIST_SUCCESS,
+  FETCH_POKEMON_LIST_FAILED
+} from "./constants";
+
+const initialState = fromJS({
+  pokemonList: [],
+  error: null,
+  isLoaded: false,
+  isLoading: false
+});
+
+function pokeFeedReducer(state = initialState, action) {
+  switch (action.type) {
+    case FETCH_POKEMON_LIST:
+      return state.set("isLoading", true);
+    case FETCH_POKEMON_LIST_SUCCESS:
+      return state
+        .set("pokemonList", action.payload)
+        .set("isLoading", false)
+        .set("isLoaded", true);
+    case FETCH_POKEMON_LIST_FAILED:
+      return state
+        .set("error", action.payload)
+        .set("isLoading", false)
+        .set("isLoaded", false);
+    default:
+      return state;
+  }
+}
+
+export default pokeFeedReducer;
